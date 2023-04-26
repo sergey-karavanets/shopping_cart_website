0a2f8d142753fa99013b110e1a197c1303989400
Sergey.Karavanets <dront22rus@mail.ru>
2023-04-26 18:28:19 +0700

    add 'Product' model with 'name' and 'price' fields

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Название товара')
+    price = models.PositiveIntegerField(verbose_name='Цена товара')
