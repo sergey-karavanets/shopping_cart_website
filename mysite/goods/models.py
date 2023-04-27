dbc3bb6559464a85acd5f566ec68e432301949e5
Sergey.Karavanets <dront22rus@mail.ru>
2023-04-27 16:47:35 +0700

    add '__str__' method in Product class

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=128, verbose_name='Название товара')
     price = models.PositiveIntegerField(verbose_name='Цена товара')
+
+    def __str__(self):
+        return f'{self.name} - {self.price} руб.'
+
+
