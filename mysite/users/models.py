d9e0b116cb65e8a4e30d4bf04f0ac6a6c2647140
Sergey.Karavanets <dront22rus@mail.ru>
2023-04-27 20:54:46 +0700

    add fields with delivery address in User model

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -3,5 +3,12 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
-    name = models.CharField(max_length=128)
-    phone = PhoneNumberField
+    name = models.CharField(max_length=128, verbose_name='Имя')
+    phone = PhoneNumberField(blank=True, verbose_name='Номер телефона', help_text='В формате: +79008007766')
+    region = models.CharField(max_length=128, verbose_name='Регион')
+    city = models.CharField(max_length=128, verbose_name='Город')
+    street_name = models.CharField(max_length=128, verbose_name='Улица')
+    house_number = models.CharField(max_length=8, verbose_name='Номер дома')
+    case_number = models.CharField(max_length=8, blank=True, verbose_name='Корпус')
+    apartment_number = models.CharField(max_length=8, verbose_name='Номер квартиры')
+
