332fe7352c4eae6ab75fe6b8449194b0c72a19d8
Sergey.Karavanets <dront22rus@mail.ru>
2023-04-28 15:14:13 +0700

    add 'product_list' and 'product_detail' funcs in goods.views.py

--- mysite/goods/views.py
+++ mysite/goods/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from cart.forms import CartAddProductForm
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = get_list_or_404(Product)
+    return render(request, 'goods/product/list.html', {'products': products})
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'goods/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
