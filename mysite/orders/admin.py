7e666b64faed131920f6a2256b8075c4927d14c1
Sergey.Karavanets <dront22rus@mail.ru>
2023-04-28 19:54:44 +0700

    register 'Order' and 'OrderItem' in admin

--- mysite/orders/admin.py
+++ mysite/orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'region', 'city',
+                    'street_name', 'house_number', 'case_number',
+                    'apartment_number', 'postal_code']
+    list_filter = ['paid']
+    inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)
